55c5ca1ab426e0f628df26cd017287203cf69bdc
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-05-04 12:00:23 +0300

    add toggle Theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React,{useState, useEffect} from "react";
 import './App.css';
 import { FaRoute } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className='title'>
-      <h1>
-        <FaRoute/>The Traveler
-      </h1>
-    </div>
 
+function App() {
+  const [appClass, setAppClass] = useState(true)
+
+  const handleClick = () => {
+    if (appClass === 'light') {
+    setAppClass('dark');
+    } else {
+    setAppClass('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = appClass;
+    }, [appClass]);
+
+  return (
+    <div className={`App ${appClass}`}>
+      <div className='title'>
+        <h1>
+          <FaRoute/>The Traveler
+        </h1>
+        <button className="theme" onClick={handleClick}>Toggle Theme</button>
+      </div>
+    </div>
   );
 }
 
